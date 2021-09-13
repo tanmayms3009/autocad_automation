51041d49d30f20d474ee83a61edcd0e1ac1fc292
Tanmay <tanmayms3009@gmail.com>
2021-09-13 13:35:37 +0530

    Polygon code

--- Polygon.py
+++ Polygon.py
@@ -1,4 +1,5 @@
 from array import array
+#os.X_OK: Checks if path can be executed.
 from os import X_OK
 from pyautocad import Autocad, APoint, aDouble
 from math import *
@@ -12,6 +13,7 @@ na = int(input("Enter the number of angles / vertices for the n-gone: "))
 #Center
 cc = input("Enter x, y coordinates for center of circle with comma separators e.g. x, y: ")
 ccc = (map(float, cc.split(", ")))
+#Converting list to tuple as pyautocad accepts tuple as input
 ccct = tuple(ccc)
 print('The coordinates for the center of circle : ',ccct)
 #Radius
@@ -19,6 +21,7 @@ rc = float(input("Enter the radius of Circle: "))
 
 #3. Calculate coordinates
 i=0
+#Creating an empty list for coordinates
 pgonc=[]
 for i in range(na):
     x=round(ccct[0]+rc*cos(2*pi*i/na),2)
@@ -30,13 +33,16 @@ for i in range(na):
 #Addind first point again to complete the loop of polygon
 fp = [pgonc[0], pgonc[1], pgonc[2]]
 pgonc.extend(fp)
-print(pgonc)
 #Converting list to tuple as pyautocad accepts tuple as input
 pgont=tuple(pgonc)
-
+print("Coordinates of Polygon: ")
+print(pgont)
 
 #4.Draw Polygon
 #To specify coordinates of a polygon we need to use aDuble method:
 polygon = aDouble(pgont)
 #To draw polygon we need to use AddPolyline method:
-acad.model.AddPolyline(polygon)
+polygond = acad.model.AddPolyline(polygon)
+#Rotate the Polygon
+polygond = polygond.Rotate(APoint(100, 25, 0), 3.14*185/180)
+
